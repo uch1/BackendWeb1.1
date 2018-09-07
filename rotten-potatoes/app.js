9e52a1b5011c7928cd126dfb47f9041fe9687501
Uchenna Aguocha <uk.aguocha@gmail.com>
2018-09-06 21:18:30 -0700

    Users can see all reviews

--- rotten-potatoes/app.js
+++ rotten-potatoes/app.js
@@ -1,6 +1,24 @@
+// PROPERTIES
+
 const express = require('express')
 const exphbs = require('express-handlebars')
 const app = express()
+
+let reviews = [
+  { title: "Uchenna is the best book ever",
+    movieTitle: "Uchenna"
+  },
+  { title: "I like cookies",
+    movieTitle: "cookies"
+  }
+]
+
+// INDEX
+app.get('/', function(req, res) {
+  res.render('layouts/reviews-index', { reviews: reviews })
+})
+
+// METHODS
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}))
 app.set('view engine', 'handlebars')
@@ -9,9 +27,9 @@ app.get('/', function(req, res) {
   res.send('Hello World!')
 })
 
-app.get('/', function(req, res) {
-  res.render('layouts/home', { msg: 'Hello World!'})
-})
+// app.get('/', function(req, res) {
+//   res.render('layouts/home', { msg: 'Hello World!'})
+// })
 
 app.listen(3000, function() {
   console.log('App listening on port 3000!')
